4feb144d1e26c86372e6766b6911c132f8d5f326
Syed Samroze Ali <samroze3@gmail.com>
2024-07-27 22:11:22 +0500

    Votes Model

---
+++ model/votes-model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const votesSchema = new mongoose.Schema({
+    voterId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Users",
+        require: true
+    },
+    reportId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Reports",
+        require: true
+    }
+}, { timestamps: true })
+
+const votesModel = mongoose.model("Votes", votesSchema);
+
+export default votesModel;
