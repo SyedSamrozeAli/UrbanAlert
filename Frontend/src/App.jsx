a815bd2ce1260f6a153c3e5533865e1e88007e85
Syed Samroze Ali <samroze3@gmail.com>
2024-08-15 04:38:08 +0500

    All the routes are now added in App.jsx from LandngPage.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,34 @@
-import { LandingPage } from "./pages/LandingPage";
-function App() {
-  return <LandingPage />;
-}
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Features } from "./pages/Features";
+import { Contact } from "./pages/Contact";
+import { HowToUse } from "./pages/HowToUse";
+import { Login } from "./pages/login";
+import { Register } from "./pages/Register";
+import { UserDashboardLayout } from "./Layouts/UserDashboardLayout";
+import { UserDashboard } from "./pages/UserDashboard";
 
-export default App;
+export const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/features" element={<Features />} />
+          <Route path="/howToUse" element={<HowToUse />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/user/:userId/dashboard"
+            element={<UserDashboardLayout />}
+          >
+            <Route path="" element={<UserDashboard />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
