cee5b0086a8918921cd9b68d15a435ceb785d480
Syed Samroze Ali <samroze3@gmail.com>
2024-08-15 01:23:11 +0500

    Landing page added

---
+++ Frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./Home";
+import { About } from "./About";
+import { Features } from "./Features";
+import { Contact } from "./Contact";
+import { HowToUse } from "./HowToUse";
+import { Login } from "./login";
+import { Register } from "./Register";
+
+export const LandingPage = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/features" element={<Features />} />
+          <Route path="/howToUse" element={<HowToUse />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
