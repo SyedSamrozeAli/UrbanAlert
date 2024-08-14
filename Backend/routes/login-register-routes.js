c008f7a9835f81241d5f3eb08873204559b2536b
Syed Samroze Ali <samroze3@gmail.com>
2024-08-15 01:27:58 +0500

    Login Registration Routes added

---
+++ Backend/routes/login-register-routes.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+import {
+
+    handleLoginUser,
+    handleRegisterUser,
+
+} from "../controllers/login-register-controllers.js";
+
+
+const router = express.Router();
+
+router.post('/login', handleLoginUser);
+
+router.post('/register', handleRegisterUser);
+
+export default router;
