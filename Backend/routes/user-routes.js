c194e1b2a7704ffa7d7bb2907e3b0dd6356baadc
Syed Samroze Ali <samroze3@gmail.com>
2024-08-16 04:58:18 +0500

    User Routes added

---
+++ Backend/routes/user-routes.js
@@ -0,0 +1,8 @@
+import express, { Router } from "express";
+import { getUserData } from "../controllers/user-controller.js";
+
+const router = express.Router();
+
+router.get('/', getUserData);
+
+export default router;
