06240f7ea1915a87b0bdd652b3be2630c2e1b466
Syed Samroze Ali <samroze3@gmail.com>
2024-08-16 05:06:06 +0500

    Loading Component added

---
+++ Frontend/src/Components/Loading.jsx
@@ -0,0 +1,11 @@
+import ".././styles/dashboard.css";
+
+export const Loading = () => {
+  return (
+    <>
+      <div className="loading-modal">
+        <p>Loading...</p>
+      </div>
+    </>
+  );
+};
