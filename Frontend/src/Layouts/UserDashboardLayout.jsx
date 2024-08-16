561aac290c3ca85babdceaff6466b579e030d848
Syed Samroze Ali <samroze3@gmail.com>
2024-08-17 00:34:03 +0500

    Reports API added

--- Frontend/src/Layouts/UserDashboardLayout.jsx
+++ Frontend/src/Layouts/UserDashboardLayout.jsx
@@ -2,6 +2,7 @@ import { Sidebar } from "../Components/Sidebar";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { setCurrentUser } from "../Slices/userSlice";
+import { setAllReports, setCurrentUserReports } from "../Slices/reportSlice";
 import { useDispatch } from "react-redux";
 import { DashboardNavbar } from "../Components/DashboardNavbar";
 import { Loading } from "../Components/Loading";
@@ -17,17 +18,32 @@ export const UserDashboardLayout = () => {
     setLoading(true);
     setError(false);
 
+    // fetching current user data from the server
     axios
       .get(`/api/user`)
       .then((response) => {
-        // getting the user data from our response
-        dispatch(setCurrentUser(response.data.data));
-        console.log(response);
+        // getting the user data from our response and saving it in state
+        console.log("Response: ", response);
+        dispatch(setCurrentUser(response.data.data.userData));
+        // Setting the reports of current user in state
+        dispatch(setCurrentUserReports(response.data.data.userData.reports));
+
         setLoading(false);
       })
       .catch((error) => {
         setError(error.message);
         setLoading(false);
+      });
+
+    // fetching all reports from server
+    axios
+      .get("/api/reports")
+      .then((response) => {
+        console.log("Reports: ", response);
+        dispatch(setAllReports(response.data.data.reports));
+      })
+      .catch((error) => {
+        setError(error.message);
       });
   }, [dispatch]);
 
