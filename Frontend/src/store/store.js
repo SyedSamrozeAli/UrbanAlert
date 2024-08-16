67e615246984f844aa525873f427fe16e1c1f7b2
Syed Samroze Ali <samroze3@gmail.com>
2024-08-17 00:35:43 +0500

    reports reducer added

--- Frontend/src/store/store.js
+++ Frontend/src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../Slices/userSlice";
+import reportsReducer from "../Slices/reportSlice";
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
+        reports: reportsReducer,
     },
     devTools: true
 });
