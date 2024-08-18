5595e96c76ebaf266978aca7029518aad0f240bf
Syed Samroze Ali <samroze3@gmail.com>
2024-08-18 05:36:44 +0500

    Get all reports API removed

--- Frontend/src/Layouts/UserDashboardLayout.jsx
+++ Frontend/src/Layouts/UserDashboardLayout.jsx
@@ -2,7 +2,6 @@ import { Sidebar } from "../Components/Sidebar";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { setCurrentUser } from "../Slices/userSlice";
-import { setAllReports, setCurrentUserReports } from "../Slices/reportSlice";
 import { useDispatch } from "react-redux";
 import { DashboardNavbar } from "../Components/DashboardNavbar";
 import { Loading } from "../Components/Loading";
@@ -23,27 +22,16 @@ export const UserDashboardLayout = () => {
       .get(`/api/user`)
       .then((response) => {
         // getting the user data from our response and saving it in state
-        console.log("Response: ", response);
+
         dispatch(setCurrentUser(response.data.data.userData));
         // Setting the reports of current user in state
-        dispatch(setCurrentUserReports(response.data.data.userData.reports));
+        // dispatch(setCurrentUserReports(response.data.data.userData.reports));
 
         setLoading(false);
       })
       .catch((error) => {
         setError(error.message);
         setLoading(false);
-      });
-
-    // fetching all reports from server
-    axios
-      .get("/api/reports")
-      .then((response) => {
-        console.log("Reports: ", response);
-        dispatch(setAllReports(response.data.data.reports));
-      })
-      .catch((error) => {
-        setError(error.message);
       });
   }, [dispatch]);
 
