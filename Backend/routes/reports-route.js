cc933b6f97a41221a4ba80f08b63f511a63034b3
Syed Samroze Ali <samroze3@gmail.com>
2024-08-18 05:36:02 +0500

    latest reports and most voted reports routes added

--- Backend/routes/reports-route.js
+++ Backend/routes/reports-route.js
@@ -1,7 +1,9 @@
 import express from "express"
-import { handleGetAllReports } from "../controllers/reports-controller.js";
+import { handleGetAllReports, handleGetLatestReports, handleGetMostVotedReports } from "../controllers/reports-controller.js";
 const router = express.Router();
 
 router.get('/', handleGetAllReports);
+router.get('/latest-reports', handleGetLatestReports);
+router.get('/most-voted-reports', handleGetMostVotedReports);
 
 export default router;
