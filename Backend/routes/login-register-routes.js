99cdfc15a90790faa79c631d2656cead578454c1
Syed Samroze Ali <samroze3@gmail.com>
2024-08-20 01:23:08 +0500

    Toastify error message pop up added

--- Backend/routes/login-register-routes.js
+++ Backend/routes/login-register-routes.js
@@ -1,16 +1,14 @@
 import express from "express";
-
+import { loginSchema, signupSchema } from "../validator/auth-validator.js";
+import { validate } from "../middleware/validate-middleware.js";
 import {
-
-    handleLoginUser,
-    handleRegisterUser,
-
+  handleLoginUser,
+  handleRegisterUser,
 } from "../controllers/login-register-controllers.js";
-
 
 const router = express.Router();
 
-router.post('/login', handleLoginUser);
-router.post('/register', handleRegisterUser);
+router.post("/login", validate(loginSchema), handleLoginUser);
+router.post("/register", validate(signupSchema), handleRegisterUser);
 
 export default router;
