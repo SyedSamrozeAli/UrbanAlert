2bcb99b68fb6d091594f302a6accd97009ea1b98
Syed Samroze Ali <samroze3@gmail.com>
2024-08-21 01:46:28 +0500

    Routes updated with new syntax

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Features } from "./pages/Features";
@@ -13,29 +13,82 @@ import { NewReportDashboard } from "./pages/user-dashboard/NewReport";
 import { MyReportDashboard } from "./pages/user-dashboard/MyReport";
 import { TrackIssuesDashboard } from "./pages/user-dashboard/TrackIssue";
 import { SettingsDashboard } from "./pages/user-dashboard/Settings";
+import { ErrorPage } from "./pages/ErrorPage";
+import { LandingPageLayout } from "./Layouts/LandingPageLayout";
 
-export const App = () => {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/features" element={<Features />} />
-          <Route path="/howToUse" element={<HowToUse />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/user/dashboard" element={<UserDashboardLayout />}>
-            <Route path="home" element={<DashboardHome />} />
-            <Route path="community" element={<CommunityDashboard />} />
-            <Route path="new-report" element={<NewReportDashboard />} />
-            <Route path="my-report" element={<MyReportDashboard />} />
-            <Route path="track-issues" element={<TrackIssuesDashboard />} />
-            <Route path="settings" element={<SettingsDashboard />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LandingPageLayout />,
+      errorElement: <ErrorPage link="/" />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/about",
+          element: <About />,
+        },
+        {
+          path: "/features",
+          element: <Features />,
+        },
+        {
+          path: "/how-to-use",
+          element: <HowToUse />,
+        },
+        {
+          path: "/contact",
+          element: <Contact />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      errorElement: <ErrorPage link="/" />,
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      errorElement: <ErrorPage link="/" />,
+      element: <Register />,
+    },
+    {
+      path: "/user/dashboard",
+      errorElement: <ErrorPage link="/user/dashboard/home" />,
+      element: <UserDashboardLayout />,
+      children: [
+        {
+          path: "home",
+          element: <DashboardHome />,
+        },
+        {
+          path: "community",
+          element: <CommunityDashboard />,
+        },
+        {
+          path: "new-report",
+          element: <NewReportDashboard />,
+        },
+        {
+          path: "my-report",
+          element: <MyReportDashboard />,
+        },
+        {
+          path: "track-issues",
+          element: <TrackIssuesDashboard />,
+        },
+        {
+          path: "settings",
+          element: <SettingsDashboard />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 };
+
+export default App;
